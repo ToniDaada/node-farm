5a5031705db102d5fa6d13a48863ec2d798cbc05
Toni Daada <tonidaada@gmail.com>
2025-07-14 16:29:16 +0100

    modified:   index.js 	new file:   templates/template-card.html 	renamed:    templates/overview.html -> templates/template-overview.html 	modified:   templates/template-product.html
    	modified:   index.js

--- index.js
+++ index.js
@@ -29,28 +29,82 @@ const path = require("path");
 
 //sERVER
 // const toni = fs.readFile(`${__dirname}/dev-data/data.json`);
-let toni;
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const dataObj = JSON.parse(data);
+
+// Functions
+const replaceTemplate = function (temp, product) {
+  let output = temp.replaceAll("{%PRODUCTNAME%}", product.productName);
+  output = output.replaceAll("{%IMAGE%}", product.image);
+  output = output.replaceAll("{%PRICE%}", product.price);
+  output = output.replaceAll("{%NUTRIENTS%}", product.nutrients);
+  output = output.replaceAll("{%QUANTITY%}", product.quantity);
+  output = output.replaceAll("{%DESCRIPTION%}", product.description);
+  output = output.replaceAll("{%FROM%}", product.from);
+  output = output.replaceAll("{%id%}", product.id);
+  if (!product.organic) {
+    output = output.replaceAll("{%NOTORGANIC%}", "not-organic");
+  }
+  return output;
+};
 
 const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  const pathName = req.url;
-  if (pathName === "/overview" || pathName === "/") {
-    res.end("Hi this is overview");
-  } else if (pathName === "/product") {
-    res.end("This is product");
-  } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      console.log(productData);
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(data);
+  const { query, pathname } = url.parse(req.url, true);
+
+  //OVERVIEW PAGE
+  if (pathname === "/overview" || pathname === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html",
     });
+
+    const cardsHtml = dataObj
+      .map((data, i) => {
+        return replaceTemplate(tempCard, data);
+      })
+      .join("");
+
+    const output = tempOverview.replaceAll("{%PRODUCTCARDS%}", cardsHtml);
+    res.end(output);
+
+    //PRODUCT PAGE
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    const product = dataObj[query.id];
+    if (product) {
+      const output = replaceTemplate(tempProduct, product);
+      res.end(output);
+    }
+
+    //API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    //NOT FOUND
   } else {
-    res.writeHead(404, { "content-type": "text/html" });
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
     res.end("<h1>Page not found</h1>");
   }
-
-  // res.end("Hi I am a server");
 });
 
 server.listen(8000, "127.0.0.1", () => {
